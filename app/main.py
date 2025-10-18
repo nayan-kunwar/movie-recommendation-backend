9d8f672ebc08d18ead8a11aa2552d28f52a4540c
Nayan Kunwar <nayankunwar678@gmail.com>
2025-10-18 10:48:53 +0530

    cors fix

--- app/main.py
+++ app/main.py
@@ -10,7 +10,8 @@ app = FastAPI(title="Movie Recommendation API")
 # Allow frontend origin
 origins = [
     "http://localhost:5173",   # React dev server
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
+    "https://movie-recommendation-frontend-one.vercel.app"
 ]
 
 app.add_middleware(
